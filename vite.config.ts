8fcd1222fc59ca6369bb34e54c9b732b9a56e224
JD <metaphyser@gmail.com>
2025-07-16 09:40:29 +0100

    trying to add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/turn-by-turn-viz/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
